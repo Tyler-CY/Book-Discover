de396195fb40889a76ee6c81f9f2a9f6b2f78902
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-02 17:51:51 -0400

    Bound viewmodel to fragment

--- app/src/main/java/com/example/bookdiscover/QueryFragment.kt
+++ app/src/main/java/com/example/bookdiscover/QueryFragment.kt
@@ -1,31 +1,41 @@
 package com.example.bookdiscover
 
-import androidx.lifecycle.ViewModelProvider
+import android.os.Build.VERSION_CODES.Q
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.bookdiscover.databinding.QueryFragmentBinding
 
 class QueryFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = QueryFragment()
-    }
+    // Default code
+//    companion object {
+//        fun newInstance() = QueryFragment()
+//    }
+//
+//    private lateinit var viewModel: QueryViewModel
 
-    private lateinit var viewModel: QueryViewModel
-
+    private val viewModel by viewModels<QueryViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.query_fragment, container, false)
+        val binding = QueryFragmentBinding.inflate(inflater)
+
+        // QueryFragment determines the lifecycle of the binding.
+        binding.lifecycleOwner = this
+
+        // Give the binding access to the QueryViewModel class
+        binding.viewModel = viewModel
+
+
+//        return inflater.inflate(R.layout.query_fragment, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(QueryViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
 
 }
