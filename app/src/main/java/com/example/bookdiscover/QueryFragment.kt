65d18282d913d30a3e830117d12f29df17f5bc1e
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-02 16:36:59 -0400

    Created fragment template

---
+++ app/src/main/java/com/example/bookdiscover/QueryFragment.kt
@@ -0,0 +1,31 @@
+package com.example.bookdiscover
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class QueryFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = QueryFragment()
+    }
+
+    private lateinit var viewModel: QueryViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.query_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(QueryViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
