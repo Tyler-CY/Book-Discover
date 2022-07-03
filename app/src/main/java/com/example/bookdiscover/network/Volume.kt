3cb8b171af6e3bf8c2bee9cee6ffdf8f5c8743d4
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-02 22:36:38 -0400

    Removed adapter for now

--- app/src/main/java/com/example/bookdiscover/network/Volume.kt
+++ app/src/main/java/com/example/bookdiscover/network/Volume.kt
@@ -3,6 +3,7 @@ package com.example.bookdiscover.network
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.JsonClass
 import com.squareup.moshi.JsonQualifier
+import com.squareup.moshi.JsonWriter
 import com.squareup.moshi.ToJson
 import org.jetbrains.annotations.Nullable
 
@@ -15,12 +16,12 @@ annotation class VolumeInfoTitle
 data class Volume (
     val id: String,
     val volumeInfo: Map<String, Any?>,
-    @VolumeInfoTitle val title: String
+//    @VolumeInfoTitle val title: String
 )
 
 // TODO: toJson not working: com.squareup.moshi.JsonDataException: Required value 'title' missing at $.items[1]
 class VolumeInfoAdapter {
-    @ToJson fun toJson(@VolumeInfoTitle title: String): String {
+    @ToJson fun toJson(title: String): String {
         return title
     }
     @FromJson @VolumeInfoTitle fun fromJson(volumeInfo: Map<String, Any?>): String {
