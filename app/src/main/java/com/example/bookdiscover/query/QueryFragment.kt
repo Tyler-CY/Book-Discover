96f60e6d666f1c63911bdfdd39025e6a36c045b8
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-03 23:05:02 -0400

    Renamed query_fragment to fragment_query

--- app/src/main/java/com/example/bookdiscover/query/QueryFragment.kt
+++ app/src/main/java/com/example/bookdiscover/query/QueryFragment.kt
@@ -1,13 +1,12 @@
-package com.example.bookdiscover
+package com.example.bookdiscover.query
 
-import android.os.Build.VERSION_CODES.Q
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.example.bookdiscover.databinding.QueryFragmentBinding
+import com.example.bookdiscover.databinding.FragmentQueryBinding
 
 class QueryFragment : Fragment() {
 
@@ -19,11 +18,12 @@ class QueryFragment : Fragment() {
 //    private lateinit var viewModel: QueryViewModel
 
     private val viewModel: QueryViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = QueryFragmentBinding.inflate(inflater)
+        val binding = FragmentQueryBinding.inflate(inflater)
 
         // QueryFragment determines the lifecycle of the binding.
         binding.lifecycleOwner = this
@@ -31,11 +31,7 @@ class QueryFragment : Fragment() {
         // Give the binding access to the QueryViewModel class
         binding.viewModel = viewModel
 
-
 //        return inflater.inflate(R.layout.query_fragment, container, false)
         return binding.root
     }
-
-
-
 }
