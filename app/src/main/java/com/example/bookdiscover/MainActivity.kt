ff64cb35b6ecb5b86ec1565fc611369c4cd3120f
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-05 00:27:46 -0400

    Added navController

--- app/src/main/java/com/example/bookdiscover/MainActivity.kt
+++ app/src/main/java/com/example/bookdiscover/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.bookdiscover
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Navigation Controllers
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_fragment_container) as androidx.navigation.fragment.NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Show a title in the app bar based off of the destination's label, and display the Up button whenever you're not on a top-level destination.
+        setupActionBarWithNavController(navController)
     }
 }
