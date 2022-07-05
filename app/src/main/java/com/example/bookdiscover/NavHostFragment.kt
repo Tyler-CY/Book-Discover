ad3bf7960c5e5d970243dd08d486a15f357d27d2
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-04 23:48:11 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/bookdiscover/NavHostFragment.kt
@@ -0,0 +1,23 @@
+package com.example.bookdiscover
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavHost
+
+class NavHostFragment : androidx.navigation.fragment.NavHostFragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nav_host, container, false)
+    }
+
+
+}
