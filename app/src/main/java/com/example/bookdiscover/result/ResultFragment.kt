81f4d9952238c1d876a2a2a46585b54c8e6e3db6
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-07 19:41:00 -0400

    Removed empty lines

--- app/src/main/java/com/example/bookdiscover/result/ResultFragment.kt
+++ app/src/main/java/com/example/bookdiscover/result/ResultFragment.kt
@@ -6,9 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import com.example.bookdiscover.databinding.FragmentResultBinding
-import com.example.bookdiscover.network.Volume
 
 class ResultFragment : Fragment() {
 
@@ -31,21 +29,13 @@ class ResultFragment : Fragment() {
         }
 
 
-        sharedViewModel.items.observe(this, Observer<List<Volume>>{
+        sharedViewModel.items.observe(this) {
             val dataset = sharedViewModel.items.value!!
 
-//        val dataset = listOf(Volume("1"), Volume("2"), Volume("3"),Volume("4"),Volume("5"),
-//            Volume("6"),Volume("7"),Volume("8"),Volume("9"),Volume("0"), Volume("A1"), Volume("A2"), Volume("A3"),Volume("A4"),Volume("A5"),
-//            Volume("A6"),Volume("A7"),Volume("A8"),Volume("A9"),Volume("A0"))
-
             val recyclerView = binding.recyclerView
-            recyclerView.adapter = ResultAdapter(dataset)
+            recyclerView.adapter = ResultAdapter(this@ResultFragment.activity!!, dataset)
             recyclerView.setHasFixedSize(true)
-        })
-
-
-
-
+        }
 
 
         // Inflate the layout for this fragment
