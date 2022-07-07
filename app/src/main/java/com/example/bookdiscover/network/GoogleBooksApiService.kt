9f70d7beaf630efadc029a60a16fd5e3541b2ca9
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-07 19:39:44 -0400

    Removed unused functions

--- app/src/main/java/com/example/bookdiscover/network/GoogleBooksApiService.kt
+++ app/src/main/java/com/example/bookdiscover/network/GoogleBooksApiService.kt
@@ -1,7 +1,6 @@
 package com.example.bookdiscover.network
 
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.addAdapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -31,17 +30,6 @@ private val retrofit = Retrofit.Builder()
  * Public interface for retrieving data from Google's Books API
  */
 interface GoogleBooksApiService {
-    /**
-     * Returns a list of books (TODO)
-     */
-    @GET("books/v1/volumes")
-    suspend fun getBooks(): VolumeQueryResult
-
-    /**
-     * Test link with some results
-     */
-    @GET("v1/volumes?q=How%20to%20build%20a%20car&maxResults=2")
-    suspend fun testLink(): VolumeQueryResult
 
     @GET("v1/volumes")
     suspend fun searchByName(@Query("q") bookName: String, @Query("maxResults") maxResults: String): VolumeQueryResult
