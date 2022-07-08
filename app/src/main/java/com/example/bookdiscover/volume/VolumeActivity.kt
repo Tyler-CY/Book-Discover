36984968e72d8fcbaff0ebd7e65e52eb2bfd0795
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-07 22:48:59 -0400

    Tested fake volumeViewModel

--- app/src/main/java/com/example/bookdiscover/volume/VolumeActivity.kt
+++ app/src/main/java/com/example/bookdiscover/volume/VolumeActivity.kt
@@ -2,8 +2,11 @@ package com.example.bookdiscover.volume
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.bookdiscover.R
+import com.example.bookdiscover.network.Volume
+import com.example.bookdiscover.result.ResultViewModel
 
 class VolumeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +18,20 @@ class VolumeActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         // Show a title in the app bar based off of the destination's label, and display the Up button whenever you're not on a top-level destination.
         setupActionBarWithNavController(navController)
+
+        val resultViewModel: ResultViewModel by viewModels()
+        val volumeViewModel: VolumeViewModel by viewModels()
+
+
+        val volumeInfo = LinkedHashMap<String, String>()
+        volumeInfo["author"] = "me"
+        volumeInfo["title"] = "my title"
+        val saleInfo = LinkedHashMap<String, String>()
+        saleInfo["profit"] = "5000"
+        val sampleVolume = Volume("fake_id", volumeInfo, null, saleInfo)
+        volumeViewModel.initialize(sampleVolume)
+
+
+
     }
 }
