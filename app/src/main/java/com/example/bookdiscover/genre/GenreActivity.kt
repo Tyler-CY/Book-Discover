7a94856ed3421dde54ecdd67434029e950ea8842
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-09 23:21:12 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/bookdiscover/genre/GenreActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bookdiscover.genre
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookdiscover.R
+
+class GenreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_genre)
+
+    }
+}
