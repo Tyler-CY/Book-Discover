6408872fa7ea5026f6d02b2ce9ca0aea5f210886
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-11 00:07:48 -0400

    Added listener to imageView of genre item

--- app/src/main/java/com/example/bookdiscover/genre/GenreAdapter.kt
+++ app/src/main/java/com/example/bookdiscover/genre/GenreAdapter.kt
@@ -1,23 +1,27 @@
 package com.example.bookdiscover.genre
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.bookdiscover.QUERY_STRING
 import com.example.bookdiscover.R
+import com.example.bookdiscover.result.ResultActivity
 import java.util.*
 
-class GenreAdapter (
+class GenreAdapter(
     private val fragmentActivity: FragmentActivity
 
-): RecyclerView.Adapter<GenreAdapter.ItemViewHolder>(){
+) : RecyclerView.Adapter<GenreAdapter.ItemViewHolder>() {
 
     val dataset = fragmentActivity.resources.getStringArray(R.array.subjects).toList() as List<String>
 
-    class ItemViewHolder(view: View): RecyclerView.ViewHolder(view){
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val imageView: ImageView = view.findViewById(R.id.genre_image)
         val textView: TextView = view.findViewById(R.id.genre_text)
     }
@@ -34,6 +38,12 @@ class GenreAdapter (
         val item = dataset[position]
 
         holder.textView.text = item
+
+        holder.imageView.setOnClickListener {
+            val intent = Intent(fragmentActivity, ResultActivity::class.java)
+            intent.putExtra(QUERY_STRING, "subject:" + holder.textView.text)
+            fragmentActivity.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
