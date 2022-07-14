e1f9fc1abd117b3fdf6a277d9f70a34ca34daa2e
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-13 21:19:29 -0400

    Added documentation

--- app/src/main/java/com/example/bookdiscover/genre/GenreImageViewModelFactory.kt
+++ app/src/main/java/com/example/bookdiscover/genre/GenreImageViewModelFactory.kt
@@ -3,6 +3,10 @@ package com.example.bookdiscover.genre
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
+/**
+ * The Factory for GenreImageViewModel
+ * This class is needed because we want to pass in a parameter to the ViewModel
+ */
 class GenreImageViewModelFactory(private val genreList: List<String>): ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return GenreImageViewModel(genreList) as T
