8328c730d0a046cfc29c691d6b010180b509aa5b
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-21 17:35:44 -0400

    (Temp) remove insert function

--- app/src/main/java/com/example/bookdiscover/database/LibraryDao.kt
+++ app/src/main/java/com/example/bookdiscover/database/LibraryDao.kt
@@ -1,6 +1,7 @@
 package com.example.bookdiscover.database
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import com.example.bookdiscover.network.Volume
 
@@ -9,4 +10,7 @@ import com.example.bookdiscover.network.Volume
 interface LibraryDao {
     @Query("SELECT * FROM library")
     fun getAll(): List<Bookmarks>
+
+    @Insert
+    fun insert(bookmarks: Bookmarks)
 }
