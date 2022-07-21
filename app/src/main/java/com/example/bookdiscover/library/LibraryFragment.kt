59d4c2b51478c2d66ef5dd618b9dd823ab3ede94
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-21 17:34:55 -0400

    Added RecyclerView handling

--- app/src/main/java/com/example/bookdiscover/library/LibraryFragment.kt
+++ app/src/main/java/com/example/bookdiscover/library/LibraryFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.bookdiscover.R
+import com.example.bookdiscover.database.AppDatabase
+import com.example.bookdiscover.database.LibraryDao
 import com.example.bookdiscover.databinding.FragmentGenreBinding
 import com.example.bookdiscover.databinding.FragmentLibraryBinding
 import com.example.bookdiscover.genre.GenreAdapter
@@ -15,8 +18,10 @@ import com.example.bookdiscover.network.Volume
 
 class LibraryFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val sharedViewModel: LibraryViewModel by activityViewModels {
+        LibraryViewModelFactory(
+            AppDatabase.getDatabase(activity!!).libraryDao()
+        )
     }
 
     override fun onCreateView(
@@ -30,7 +35,14 @@ class LibraryFragment : Fragment() {
         binding.apply {
             // ResultViewModel determines the lifecycle of the binding.
             lifecycleOwner = viewLifecycleOwner
+
+            viewModel = sharedViewModel
         }
+
+        val recyclerView = binding.libraryRecyclerView
+        recyclerView.adapter = LibraryAdapter(this@LibraryFragment.activity!!, listOf(Volume("1"), Volume("1"),Volume("1"),Volume("1"),Volume("1")))
+        recyclerView.setHasFixedSize(true)
+
 
         // Inflate the layout for this fragment
         return binding.root
