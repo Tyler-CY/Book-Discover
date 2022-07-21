ae28208759beba8096af3c426027dd8b3e3ad774
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-21 17:34:23 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/bookdiscover/library/LibraryAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.bookdiscover.library
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookdiscover.R
+import com.example.bookdiscover.network.Volume
+
+
+class LibraryAdapter(
+    // Referenced activity is used to start intent
+    private val fragmentActivity: FragmentActivity,
+    // A List of Volume objects, returned by GoogleBooksApi
+    private val dataset: List<Volume>
+) : RecyclerView.Adapter<LibraryAdapter.ItemViewHolder>() {
+
+    // ViewHolder used in the RecyclerView
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleView: TextView = view.findViewById(R.id.item_title)
+        val authorView: TextView = view.findViewById(R.id.item_author)
+        val bookView: ImageView = view.findViewById(R.id.book_cover)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // Inflate the layout
+        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.result_item, parent, false)
+
+        // Returns the ItemViewHolder after inflating the layout
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.titleView.text = "Position" + position.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+}
