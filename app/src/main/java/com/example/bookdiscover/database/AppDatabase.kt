a33f121818e5a4223b0ba589b430c9047e7a20ee
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-21 20:07:27 -0400

    Added callback for initialization

--- app/src/main/java/com/example/bookdiscover/database/AppDatabase.kt
+++ app/src/main/java/com/example/bookdiscover/database/AppDatabase.kt
@@ -4,28 +4,52 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.bookdiscover.network.Volume
 
 @Database(entities = arrayOf(Bookmarks::class), exportSchema = false, version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun libraryDao(): LibraryDao
 
-    companion object {
 
+
+
+
+
+    companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
         fun getDatabase(context: Context): AppDatabase{
+
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context,
                     AppDatabase::class.java,
                     "app_database")
+                    .addCallback(object : Callback(){
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            Thread { prepopulateDb(getDatabase(context)) }.start()
+                        }
+                    })
                     .build()
                 INSTANCE = instance
 
                 instance
             }
+        }
+
+
+        private val CALLBACK = object: RoomDatabase.Callback() {
+
+        }
+
+        private fun prepopulateDb(db: AppDatabase){
+            val bookmark1 = Bookmarks("Qk4rDwAAQBAJ")
+            val bookmark2 = Bookmarks("Hht2CgAAQBAJ")
+            db.libraryDao().insert(bookmark1)
+            db.libraryDao().insert(bookmark2)
         }
     }
 }
