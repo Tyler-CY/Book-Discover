04f0bec9423071991e10206e95ccd2b776d0c585
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-21 20:09:35 -0400

    Fixed ViewModel with CoroutineScope

--- app/src/main/java/com/example/bookdiscover/library/LibraryViewModel.kt
+++ app/src/main/java/com/example/bookdiscover/library/LibraryViewModel.kt
@@ -1,12 +1,29 @@
 package com.example.bookdiscover.library
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.bookdiscover.database.Bookmarks
 import com.example.bookdiscover.database.LibraryDao
 import com.example.bookdiscover.network.Volume
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class LibraryViewModel(private val libraryDao: LibraryDao): ViewModel() {
-    fun getAllSavedVolumes(): List<Bookmarks> {
-        return libraryDao.getAll()
+
+    private val _bookmarks = MutableLiveData<List<Bookmarks>>()
+    val bookmarks: LiveData<List<Bookmarks>> = _bookmarks
+
+    init{
+        getAllSavedVolumes()
+    }
+
+    fun getAllSavedVolumes() {
+        CoroutineScope(Dispatchers.IO).launch {
+//            _bookmarks.value = libraryDao.getAll()
+            _bookmarks.postValue(libraryDao.getAll())
+        }
     }
 }
