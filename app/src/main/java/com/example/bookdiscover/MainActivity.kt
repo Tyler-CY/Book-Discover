d3a7d90e31bff86437ab616d76132825593d1f5b
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-29 00:02:58 -0400

    Added toolbar

--- app/src/main/java/com/example/bookdiscover/MainActivity.kt
+++ app/src/main/java/com/example/bookdiscover/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.bookdiscover
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 import com.example.bookdiscover.genre.GenreActivity
 import com.example.bookdiscover.library.LibraryActivity
 import com.example.bookdiscover.search.SearchActivity
@@ -16,6 +19,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.activity_main_toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.setSubtitle("Home")
 
         // This button is responsible for starting SearchActivity
         val searchButton = findViewById<Button>(R.id.search_button)
@@ -39,6 +46,29 @@ class MainActivity : AppCompatActivity() {
         val settingsButton = findViewById<Button>(R.id.settings_button)
         settingsButton.setOnClickListener{
             Snackbar.make(it, "Settings", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_info -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.action_settings -> {
+                return true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+                return true
+            }
         }
     }
 }
