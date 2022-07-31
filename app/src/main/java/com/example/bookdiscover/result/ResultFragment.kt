4503b912a13966d6ed15165f6243a2a388b6bf5c
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-07-30 22:33:43 -0400

    Added loading tips

--- app/src/main/java/com/example/bookdiscover/result/ResultFragment.kt
+++ app/src/main/java/com/example/bookdiscover/result/ResultFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import com.example.bookdiscover.databinding.FragmentResultBinding
 
@@ -35,9 +38,32 @@ class ResultFragment : Fragment() {
         sharedViewModel.items.observe(this) {
             val dataset = sharedViewModel.items.value!!
 
-            val recyclerView = binding.recyclerView
-            recyclerView.adapter = ResultAdapter(this@ResultFragment.activity!!, dataset)
-            recyclerView.setHasFixedSize(true)
+            if (dataset.isNotEmpty()){
+                binding.resultLayout.removeViewAt(0)
+                val recyclerView = binding.recyclerView
+                binding.recyclerView.isVisible = true
+                recyclerView.adapter = ResultAdapter(this@ResultFragment.activity!!, dataset)
+                recyclerView.setHasFixedSize(true)
+            }
+
+            else {
+                binding.recyclerView.isVisible = false
+
+                val textView = TextView(activity!!)
+                textView.id = View.generateViewId()
+                textView.text = "Loading..."
+                textView.textSize = 24F
+                binding.resultLayout.addView(textView, 0)
+
+                // Sets the textView to the middle by using ConstraintSet
+                val constraintSet = ConstraintSet()
+                constraintSet.clone(binding.resultLayout)
+                constraintSet.connect(textView.id, ConstraintSet.TOP, binding.resultLayout.id, ConstraintSet.TOP)
+                constraintSet.connect(textView.id, ConstraintSet.START, binding.resultLayout.id, ConstraintSet.START)
+                constraintSet.connect(textView.id, ConstraintSet.END, binding.resultLayout.id, ConstraintSet.END)
+                constraintSet.connect(textView.id, ConstraintSet.BOTTOM, binding.resultLayout.id, ConstraintSet.BOTTOM)
+                constraintSet.applyTo(binding.resultLayout)
+            }
         }
 
 
